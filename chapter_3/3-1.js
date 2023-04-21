ca76eee90ef644e08e58db4dfdafd2edd07bba1d
ku-haeeun <62587097+ku-haeeun@users.noreply.github.com>
2023-04-21 15:37:04 +0900

    3-1 commir

---
+++ chapter_3/3-1.js
@@ -0,0 +1,5 @@
+console.log(this); // { alert: f(), atob: f(), blur: f(), btoa: f(), ... }
+console.log(window); // { alert: f(), atob: f(), blur: f(), btoa: f(), ... }
+console.log(this === window); // true
+
+//전역 공간에서의 this
