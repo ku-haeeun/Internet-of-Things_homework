d350bd54c9a33ba9d34eafa9251e8b99f259888e
ku-haeeun <62587097+ku-haeeun@users.noreply.github.com>
2023-04-21 13:30:22 +0900

    2-1 commit

---
+++ chapter_2/2-1.js
@@ -0,0 +1,16 @@
+// -------------------------- (1)
+var a = 1;
+function outer() {
+  function inner() {
+    console.log(a); // undefined
+    var a = 3;
+  }
+  inner(); // ------------ (2)
+  console.log(a); // 1
+}
+outer(); // ---------------- (3)
+console.log(a); // 1
+
+//실행 컨텍스트랑 콜 스택
+// declaration - 선언
+// expression - 표현
