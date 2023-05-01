68869c6a8d6b65e389554d23786b37c0004433f7
ku-haeeun <62587097+ku-haeeun@users.noreply.github.com>
2023-05-01 16:01:55 +0900

    3-22 commit

---
+++ chapter_3/3-22.js
@@ -0,0 +1,13 @@
+var numbers = [10, 20, 3, 16, 45];
+var max = (min = numbers[0]);
+numbers.forEach(function(number) {
+  if (number > max) {
+    max = number;
+  }
+  if (number < min) {
+    min = number;
+  }
+});
+console.log(max, min); // 45 3
+
+// 여러 인수를 받는 메서드에 apply를 적용.(math.max/math.min)
