b797379feb699f18bcea4a4254c3131bb5c316ca
ku-haeeun <62587097+ku-haeeun@users.noreply.github.com>
2023-05-23 16:00:59 +0900

    4-8 commit

---
+++ chapter_4/4-8.js
@@ -0,0 +1,13 @@
+var obj1 = {
+    name: 'obj1',
+    func: function() {
+      var self = this;
+      return function() {
+        console.log(self.name);
+      };
+    },
+  };
+  var callback = obj1.func();
+  setTimeout(callback, 1000);
+
+  //콜백 함수 내부의 this에 다른 값을 바인딩하는 방법 -> 전통적인 방식
