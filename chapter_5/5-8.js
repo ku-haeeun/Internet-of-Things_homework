ae16224ef3786fdc5f95c5fa9d55dc4378252af7
ku-haeeun <62587097+ku-haeeun@users.noreply.github.com>
2023-05-23 16:22:40 +0900

    5-8 commit

---
+++ chapter_5/5-8.js
@@ -0,0 +1,15 @@
+var fruits = ['apple', 'banana', 'peach'];
+var $ul = document.createElement('ul');
+
+var alertFruit = function(fruit) {
+  alert('your choice is ' + fruit);
+};
+fruits.forEach(function(fruit) {
+  var $li = document.createElement('li');
+  $li.innerText = fruit;
+  $li.addEventListener('click', alertFruit.bind(null, fruit));
+  $ul.appendChild($li);
+});
+document.body.appendChild($ul);
+
+// 고차함수 활용 - 클로저 적극 활용
