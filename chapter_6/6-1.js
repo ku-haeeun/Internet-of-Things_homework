e30f1343d55912161772a755b26caa78d96df89a
ku-haeeun <62587097+ku-haeeun@users.noreply.github.com>
2023-06-09 16:15:11 +0900

    6-1 commit

--- chapter_6/6-1.js
+++ chapter_6/6-1.js
@@ -4,7 +4,9 @@ var Person = function(name) {
   Person.prototype.getName = function() {
     return this._name;
   };
-
+  var suzi = new Person("Suzi");
+  suzi.__proto__.getName(); // undefined
+  suzi.getName(); // Suzi
 
   // 프로토 타입
   // this 를 사용해서 프로토타입을 확인한다.
